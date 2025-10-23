1b36950eab71a2f3f91d6fd8007f003a0853bb40
Tyler Coudriet <tyler.coudriet@gmail.com>
2025-10-23 12:12:46 -0500

    correct build step

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,11 @@ import tailwindcss from '@tailwindcss/vite';
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-const APP_ID = 0;
-
 // https://astro.build/config
 export default defineConfig({
 	outDir: './app/assets',
 	build: {
-		assetsPrefix: `/${APP_ID}/assets/`
+		assetsPrefix: `./`
 	},
 	vite: {
 		build: {
